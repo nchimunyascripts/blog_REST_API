f3e1c77a62c58f8b0297f4b6ab4ed027c6ede100
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2024-11-29 04:25:40 +0200

    Schemas

--- app/main.py
+++ app/main.py
@@ -1,25 +1,18 @@
 """Fast API"""
-import time
 from uuid import uuid4
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from pydantic import BaseModel
 import psycopg2
 from sqlalchemy.orm import Session
 from psycopg2.extras import RealDictCursor
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-class Post(BaseModel):
-    """POST Schema"""
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
+
 
 
 my_posts = [
@@ -27,16 +20,16 @@ my_posts = [
     {"title": "Summer Time 3", "content": "Something to die for love", "published": False, "rating": 3, "id": 5}
 ]
 
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='postgres', user='postgres',
-                                    password='0977398719', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print('Connected to PSQL')
-        break
-    except Exception as error:
-        print('Connection to database failed!\nError: ', error)
-        time.sleep(2)
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='postgres', user='postgres',
+#                                     password='0977398719', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print('Connected to PSQL')
+#         break
+#     except Exception as error:
+#         print('Connection to database failed!\nError: ', error)
+#         time.sleep(2)
 
 
 async def find_post(_id):
@@ -59,34 +52,28 @@ async def root():
     }
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 async def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {
-        "data": posts
-    }
+    return posts
 
 
-@app.post("/posts",  status_code=status.HTTP_201_CREATED)
-async def create_post(post: Post, db: Session = Depends(get_db)):
+@app.post("/posts",  status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {
-        "data": new_post
-    }
+    return new_post
 
 
-@app.get('/posts/{_id}')
+@app.get('/posts/{_id}', response_model=schemas.Post)
 async def get_post(_id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == _id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'Post with id {_id} was not found!')
-    return {
-        "Data": post
-    }
+    return post
 
 
 @app.delete('/posts/{_id}', status_code=status.HTTP_204_NO_CONTENT)
@@ -100,13 +87,13 @@ async def delete_post(_id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put('/posts/{_id}')
-async def update_post(_id: int, post: Post, db: Session = Depends(get_db)):
+@app.put('/posts/{_id}', response_model=schemas.Post)
+async def update_post(_id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.id == _id)
     post = post_query.first()
     if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'Post with id {_id} does not exist')
-    post_query.update(post.dict(), synchronize_session=False)
+    post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return {'data': post_query.first()}
+    return post_query.first()
