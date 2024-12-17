be0c17301dbf655c35b41e939b68fc78893c956f
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2024-12-17 05:20:55 +0200

    cors

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 """Fast API"""
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .router import post, user, auth, vote
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+origins = ['http://127.0.0.1:800/']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"]
+)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
